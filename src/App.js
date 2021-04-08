de6b049a5ebbb52d883568d8fdc0c0ad0e571b68
Ziad <ziadmkh7@gmail.com>
2021-04-08 19:25:15 +0200

    Created Header Component and added some default styling.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import Header from './components/Header';
+import './default.scss';
+
 function App() {
   return (
     <div className="App">
-
+      <Header />
+      <h1>Hello World</h1>
     </div>
   );
 }
---
+++ src/components/Header/index.js
@@ -0,0 +1,11 @@
+import './styles.scss';
+
+const Header = props => {
+    return (
+        <header className="header">
+
+        </header>
+    );
+};
+
+export default Header;
