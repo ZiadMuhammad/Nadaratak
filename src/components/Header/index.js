0063c9a1224ec6d14b40b35e9f0a5bb149f1cce4
Ziad <ziadmkh7@gmail.com>
2021-04-09 16:02:06 +0200

    -Finished Homepage -Added Footer -Added Routing -Added Registration Page

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,4 +1,5 @@
 import './styles.scss';
+import { Link } from 'react-router-dom';
 
 import Logo from './../../assets/logo.png';
 
@@ -7,7 +8,18 @@ const Header = props => {
         <header className="header">
             <div className="wrap">
                 <div className="logo">
-                    <img src={Logo} alt="Nadaratak.com Logo"/>
+                    <Link to="/">
+                        <img src={Logo} alt="Nadaratak.com Logo"/>
+                    </Link>
+                </div>
+                <div className="callToActions">
+                    <ul>
+                        <li>
+                            <Link to="/registration">
+                                Register
+                            </Link>
+                        </li>
+                    </ul>
                 </div>
             </div>
         </header>
