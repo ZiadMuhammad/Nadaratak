933cac5c6f7ff62e73e9ac524ef271c0c7fa76ab
Ziad <ziadmkh7@gmail.com>
2021-04-12 22:27:44 +0200

    Added ML & AR Service

---
+++ NadaratakClient/src/components/Footer/index.js
@@ -0,0 +1,13 @@
+import './styles.scss';
+
+const Footer = props => {
+    return (
+        <footer className="footer">
+            <div className="wrap">
+                © Nadaratak.com 2021
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
--- README.md
+++
@@ -1,5 +0,0 @@
-# Nadaratak.com
-
-Nadaratak.com is going to be a smart ecommerce platform for buying glasses online. You can try on glasses before purchasing them and you can even upload your image to let AI choose the best glasses for you!
-
-Stay Tuned!
