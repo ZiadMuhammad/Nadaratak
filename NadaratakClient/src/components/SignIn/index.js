149ae2f116c451d50d8e0c8c17f256781bcc8417
Ziad <ziadmkh7@gmail.com>
2021-05-01 01:23:49 +0200

    Added Login

--- NadaratakClient/src/components/SignIn/index.js
+++ NadaratakClient/src/components/SignIn/index.js
@@ -1,15 +1,50 @@
 import React, { Component } from 'react';
 import './styles.scss';
 import Button from './../forms/Button';
-import { signInWithGoogle } from './../../firebase/utils.js';
+import { signInWithGoogle, auth } from './../../firebase/utils.js';
+
+import FormInput from './../forms/FormInput';
+
+const initialState = {
+    email: '',
+    password: ''
+};
 
 class SignIn extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            ...initialState
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(e) {
+        const {name, value} = e.target;
+        this.setState({
+            [name]: value
+        });
+    }
+
     handleSubmit = async e => {
         e.preventDefault();
+        const {email, password} = this.state;
+
+        try {
+            await auth.signInWithEmailAndPassword(email, password);
+            this.setState({
+                ...initialState
+            });
+        } catch(err) {
+            // console.log(err);
+        }
     }
     
     render() {
+        const {email, password} = this.state;
+
         return (
             <div className="signin">
                 <div className="wrap">
@@ -19,6 +54,24 @@ class SignIn extends Component {
     
                     <div className="formWrap">
                         <form onSubmit={this.handleSubmit}>
+                            <FormInput
+                                type="email"
+                                name="email"
+                                value={email}
+                                placeholder="Email"
+                                handleChange={this.handleChange}
+                            />
+
+                            <FormInput
+                                type="password"
+                                name="password"
+                                value={password}
+                                placeholder="Password"
+                                handleChange={this.handleChange}
+                            />
+
+                            <Button type="submit">Login</Button>
+
                             <div className="socialSignin">
                                 <div className="row">
                                     <Button onClick={signInWithGoogle}>
