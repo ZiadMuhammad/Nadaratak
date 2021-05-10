383aa7bd8c806f66379e86a3192829ebb8ccff7b
Ziad <ziadmkh7@gmail.com>
2021-05-10 11:58:54 +0200

    Added UI update after product addition functionality

--- NadaratakClient/src/pages/Admin/index.js
+++ NadaratakClient/src/pages/Admin/index.js
@@ -1,13 +1,19 @@
 import React, { useState, useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import { addProductStart } from './../../redux/Products/products.actions';
+import { useDispatch, useSelector } from 'react-redux';
+import { addProductStart, fetchProductsStart, deleteProductStart } from './../../redux/Products/products.actions';
+import { Link } from 'react-router-dom';
 import Modal from './../../components/Modal';
 import FormInput from './../../components/forms/FormInput';
 import FormSelect from './../../components/forms/FormSelect';
 import Button from './../../components/forms/Button';
 import './styles.scss';
 
+const mapState = ({ productsData }) => ({
+  products: productsData.products
+})
+
 const Admin = props => {
+    const { products } = useSelector(mapState);
     const dispatch = useDispatch();
     const [hideModal, setHideModal] = useState(true);
     const [productCategory, setProductCategory] = useState('mens');
@@ -19,12 +25,29 @@ const Admin = props => {
     const [productStock, setProductStock] = useState(0);
     const [productDesc, setProductDesc] = useState('');
 
+    useEffect(() => {
+      dispatch(
+        fetchProductsStart()
+      );
+
+    }, []);
+
   const toggleModal = () => setHideModal(!hideModal);
 
   const configModal = {
     hideModal,
     toggleModal
   };
+
+  const resetForm = () => {
+    setHideModal(true);
+    setProductCategory('mens');
+    setProductName('');
+    setCompatibleFace('oval');
+    setProductThumbnail('');
+    setTryonLink('');
+    setProductPrice(0);
+  }
 
   const handleSubmit = e => {
     e.preventDefault();
@@ -39,7 +62,9 @@ const Admin = props => {
             productPrice,
             productStock
         })
-    )
+    );
+
+    resetForm();
 
   };
 
@@ -145,6 +170,64 @@ const Admin = props => {
           </form>
         </div>
       </Modal>
+      
+      <div className="manageProducts">
+        <table border="0" cellPadding="0" cellSpacing="0">
+          <tbody>
+            <tr>
+              <th>
+                <h1 align="left">
+                  Manage Products
+                </h1>
+              </th>
+            </tr>
+            <tr>
+              <td>
+                <table className="results" border="0" cellPadding="10" cellSpacing="0">
+                  <tbody>
+                    {products.map((product, index) => {
+                      const {
+                        productName,
+                        productThumbnail,
+                        productPrice, 
+                        compatibleFace, 
+                        documentID,
+                        tryonLink
+                      } = product;
+                      return (
+                        <tr key={index}>
+                          <td>
+                            <img width="100px" src={productThumbnail}/>
+                          </td>
+                          <td>
+                            {productName}
+                          </td>
+                          <td>
+                            LE {productPrice}
+                          </td>
+                          <td>
+                            {compatibleFace}
+                          </td>
+                          <td>
+                            {tryonLink ? 
+                            <a target="_blank" href={tryonLink}>Try</a>
+                            : <p>N/A</p>}
+                          </td>
+                          <td>
+                            <Button onClick = {() => dispatch(deleteProductStart(documentID))}>
+                                Delete
+                            </Button>
+                          </td>
+                        </tr>
+                      )
+                    })}
+                  </tbody>
+                </table>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
--- NadaratakClient/src/redux/Products/products.sagas.js
+++ NadaratakClient/src/redux/Products/products.sagas.js
@@ -1,6 +1,7 @@
 import { auth } from './../../firebase/utils';
 import { takeLatest, put, all, call } from 'redux-saga/effects';
-import { handleAddProduct } from './products.helpers';
+import { setProducts, fetchProductsStart } from './products.actions';
+import { handleAddProduct, handleFetchProducts, handleDeleteProduct } from './products.helpers';
 import productsTypes from './products.types';
 
 
@@ -26,6 +27,9 @@ export function* addProduct({ payload: {
                 productAdminUserUID: auth.currentUser.uid,
                 createdDate: timestamp
             })
+            yield put(
+                fetchProductsStart()
+            );
         } catch(err) {
 
         }
@@ -35,8 +39,40 @@ export function* onAddProductStart() {
     yield takeLatest(productsTypes.ADD_NEW_PRODUCT_START, addProduct)
 }
 
+export function* fetchProducts() {
+    try {
+        const products = yield handleFetchProducts();
+        yield put(
+            setProducts(products)
+        )
+    } catch(err) {
+        //console.log(err)
+    }
+}
+
+export function* onFetchProductsStart() {
+    yield takeLatest(productsTypes.FETCH_PRODUCTS_START, fetchProducts)
+}
+
+export function* deleteProduct({ payload }) {
+    try {
+        yield handleDeleteProduct(payload);
+        yield put(
+            fetchProductsStart()
+        );
+    } catch(err) {
+        //console.log(err);
+    }
+}
+
+export function* onDeleteProductStart() {
+    yield takeLatest(productsTypes.DELETE_PRODUCT_START, deleteProduct)
+}
+
 export default function* productsSagas() {
     yield all([
-        call(onAddProductStart)
+        call(onAddProductStart),
+        call(onFetchProductsStart),
+        call(onDeleteProductStart)
     ])
 }
